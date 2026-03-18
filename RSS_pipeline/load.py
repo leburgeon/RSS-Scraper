4a2b825b4f98607a9acc7af7691faef9172a0606
CHKSmith25 <chs.smith89@gmail.com>
2026-03-18 16:34:41 +0000

    load functions made for individual items and batch items

--- RSS_pipeline/load.py
+++ RSS_pipeline/load.py
@@ -1,3 +1,20 @@
 """
 This script contains functions for loading and preprocessing data for the RSS pipeline.
 This includes functions error handling and loading data to awsDynamoDB. """
+
+import boto3
+
+
+def insert_item(item: dict, table: boto3.resources.factory.dynamodb.Table):
+    """Insert a item into the database.
+    """
+    response = table.put_item(Item=item)
+    return response
+
+
+def insert_items(items: list[dict], table: boto3.resources.factory.dynamodb.Table):
+    """Insert multiple items into the database.
+    """
+    with table.batch_writer() as batch:
+        for item in items:
+            batch.put_item(Item=item)
