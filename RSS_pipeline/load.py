f63c7a58585feace3e299edffb2a66a31c64e372
Dominic <dominicsoutherby233@gmail.com>
2026-03-18 16:35:40 +0000

    adding metadata function

--- RSS_pipeline/load.py
+++ RSS_pipeline/load.py
@@ -1,3 +1,16 @@
 """
 This script contains functions for loading and preprocessing data for the RSS pipeline.
 This includes functions error handling and loading data to awsDynamoDB. """
+
+
+def enrich_impressions_with_article_metadata(impressions: list[dict], article: dict) -> list[dict]:
+    """Enrich impressions with article metadata. This is useful for downstream processing and analysis."""
+    
+    for impression in impressions:
+        impression['article_title'] = article.get('title')
+        impression['article_link'] = article.get('link')
+        impression['article_publish_date'] = article.get('publish_date')
+        impression['article_guid'] = article.get('guid')
+        impression['article_summary'] = article.get('summary')
+    
+    return impressions
