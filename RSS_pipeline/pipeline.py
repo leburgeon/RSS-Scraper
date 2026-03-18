4df0ae95627c4a87461057491ed8f11cdec978ed
Dominic <dominicsoutherby233@gmail.com>
2026-03-18 09:13:47 +0000

    pipeline file init

---
+++ RSS_pipeline/pipeline.py
@@ -0,0 +1,9 @@
+"""
+This script is the main pipeline for RSS extraction.
+
+This script should extract the entities from the RSS feed. 
+Then it should rate these entities 'positive', 'negative' or 'neutral' based on the sentiment of the article.
+
+If the article mentions an entity in a positive and negative light, two entries should be made.
+
+"""
