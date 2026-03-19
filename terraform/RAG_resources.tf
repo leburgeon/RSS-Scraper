ae5e5739e6bc8e094067eb128b267b15cf8ce75f
Dominic <dominicsoutherby233@gmail.com>
2026-03-19 17:17:41 +0000

    updated terraform for RAG database

--- terraform/RAG_resources.tf
+++ terraform/RAG_resources.tf
@@ -14,18 +14,11 @@ data "aws_security_group" "rss_scraper" {
   vpc_id = data.aws_vpc.c22_vpc.id
 }
 
-# Security group for Lambda to connect to RDS
+# Security group for Lambda
 resource "aws_security_group" "lambda_rds" {
   name        = "c22-lambda-rds-security-group"
   description = "Security group for Lambda to access RDS"
   vpc_id      = data.aws_vpc.c22_vpc.id
-
-  egress {
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = [aws_security_group.rds.id]
-  }
 
   tags = {
     Name = "c22-lambda-rds-security-group"
@@ -38,25 +31,40 @@ resource "aws_security_group" "rds" {
   description = "Security group for RAG RDS database"
   vpc_id      = data.aws_vpc.c22_vpc.id
 
-  ingress {
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = [data.aws_security_group.rss_scraper.id]
-    description     = "Allow from RSS scraper ECS task"
-  }
-
-  ingress {
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lambda_rds.id]
-    description     = "Allow from Lambda functions"
-  }
-
   tags = {
     Name = "c22-rag-rds-security-group"
   }
+}
+
+# Security group rules
+resource "aws_security_group_rule" "lambda_to_rds_egress" {
+  type                     = "egress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.rds.id
+  security_group_id        = aws_security_group.lambda_rds.id
+  description              = "Allow Lambda to connect to RDS"
+}
+
+resource "aws_security_group_rule" "rss_scraper_to_rds_ingress" {
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  source_security_group_id = data.aws_security_group.rss_scraper.id
+  security_group_id        = aws_security_group.rds.id
+  description              = "Allow RSS scraper ECS task to connect to RDS"
+}
+
+resource "aws_security_group_rule" "lambda_to_rds_ingress" {
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.lambda_rds.id
+  security_group_id        = aws_security_group.rds.id
+  description              = "Allow Lambda to connect to RDS"
 }
 
 # DB subnet group
@@ -66,21 +74,6 @@ resource "aws_db_subnet_group" "rag_db_subnet_group" {
 
   tags = {
     Name = "c22-rag-db-subnet-group"
-  }
-}
-
-# DB parameter group for pgvector
-resource "aws_db_parameter_group" "rag_db_params" {
-  name   = "c22-rag-db-params"
-  family = "postgres15"
-
-  parameter {
-    name  = "shared_preload_libraries"
-    value = "vector"
-  }
-
-  tags = {
-    Name = "c22-rag-db-params"
   }
 }
 
@@ -116,14 +109,13 @@ resource "aws_secretsmanager_secret_version" "rds_master_password" {
 resource "aws_db_instance" "rag_db" {
   identifier              = "c22-media-rag-db"
   engine                  = "postgres"
-  engine_version          = "15.4"
+  engine_version          = "15"
   instance_class          = "db.t3.micro"
   allocated_storage       = 20
   storage_type            = "gp3"
   db_name                 = "rag_database"
   username                = "media_group_project_RAG_DB"
   password                = random_password.rds_master_password.result
-  parameter_group_name    = aws_db_parameter_group.rag_db_params.name
   db_subnet_group_name    = aws_db_subnet_group.rag_db_subnet_group.name
   vpc_security_group_ids  = [aws_security_group.rds.id]
   publicly_accessible     = false
@@ -133,6 +125,25 @@ resource "aws_db_instance" "rag_db" {
   tags = {
     Name = "c22-media-rag-db"
   }
+}
 
-  depends_on = [aws_db_parameter_group.rag_db_params]
+# Outputs
+output "rds_endpoint" {
+  description = "RDS instance endpoint"
+  value       = aws_db_instance.rag_db.endpoint
+}
+
+output "rds_address" {
+  description = "RDS instance address (hostname only)"
+  value       = aws_db_instance.rag_db.address
+}
+
+output "secrets_manager_arn" {
+  description = "ARN of the Secrets Manager secret containing RDS credentials"
+  value       = aws_secretsmanager_secret.rds_master_password.arn
+}
+
+output "lambda_security_group_id" {
+  description = "Security group ID for Lambda functions"
+  value       = aws_security_group.lambda_rds.id
 }
