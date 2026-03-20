b27faecbd214b0f7a98a4be330e39a490a721910
Dominic <dominicsoutherby233@gmail.com>
2026-03-20 15:09:26 +0000

    upload

--- RAG_chatbot/upload_frontend.sh
+++ RAG_chatbot/upload_frontend.sh
@@ -1,3 +1,7 @@
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 129033205317.dkr.ecr.eu-west-2.amazonaws.com
 
 docker build -t c22-media-group-chatbot-frontend:latest .
+
+docker tag c22-media-group-chatbot-frontend:latest 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c22-media-group-chatbot-frontend:latest
+
+docker push 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c22-media-group-chatbot-frontend:latest
